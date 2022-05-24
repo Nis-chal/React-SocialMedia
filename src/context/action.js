599e6eeaecc0d102cdeb75a9715126668ade2b7f
Nis-chal <nischalchan@gmail.com>
2022-05-24 13:19:57 +0545

    axios setup was done

---
+++ src/context/action.js
@@ -0,0 +1,18 @@
+export const DISPLAY_ALERT = 'SHOW_ALERT'
+export const CLEAR_ALERT = 'CLEAR_ALERT'
+
+
+export const HANDLE_CHANGE = "HANDLE_CHANGE";
+export const CLEAR_VALUES = "CLEAR_VALUES";
+
+export const SETUP_USER_BEGIN = 'SETUP_USER_BEGIN'
+export const SETUP_USER_SUCCESS = 'SETUP_USER_SUCCESS'
+export const SETUP_USER_ERROR = 'SETUP_uSER_ERROR'
+
+export const LOGOUT_USER = 'LOGOUT_USER'
+
+
+
+
+export const SHOW_STATS_BEGIN = "SHOW_STATS_BEGIN";
+export const SHOW_STATS_SUCCESS = "SHOW_STATS_SUCCESS";
