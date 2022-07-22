aec7ed70401e67a5d6f0e884fa7f369994a7bcbf
Nis-chal <nischalchan@gmail.com>
2022-07-22 06:14:10 +0545

    error page route was setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Landing from "./Landing";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 import Profile from "./profilePage";
+import ErrorPage from "./404";
 
 
-export {Landing,Register,ProtectedRoute,Profile}
+export {Landing,Register,ProtectedRoute,Profile,ErrorPage}
